f817a6a7e50eea5643e4cdf15ed543caeeeb8e83
Carmine Massei <carminemassei@macbook-pro-di-carmine.local>
2018-10-10 22:59:28 +0200

    Aggiunta funzione readTrips

---
+++ Itinerary/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  Itinerary
+//
+//  Created by Carmine Massei on 10/10/18.
+//  Copyright © 2018 iOSFoundation. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    static var tripModels = [TripModel]()
+    
+}
