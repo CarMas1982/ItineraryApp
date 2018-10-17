15a4f262465934901d94752ee66be7b47eeec7c1
Carmine Massei <carminemassei@macbook-pro-di-carmine.local>
2018-10-17 22:59:27 +0200

    Aggiunta di una icona sulla right view del text field

--- Itinerary/Views/AddTripViewController/AddTripViewController.swift
+++ Itinerary/Views/AddTripViewController/AddTripViewController.swift
@@ -28,8 +28,33 @@ class AddTripViewController: UIViewController {
     }
     
     @IBAction func save(_ sender: UIButton) {
+        
+        guard tripTextField.text != "", let newTripName = tripTextField.text else {
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 30, height: 20))
+            imageView.image = #imageLiteral(resourceName: "warning") //image literal
+            imageView.contentMode = .scaleAspectFit
+            tripTextField.rightView = imageView
+            
+            
+            
+            //Alternative
+            //1: usare un color literal per evidenziare il text field
+//            tripTextField.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+            
+            //2: colorare il bordo, arrotondare i bordi del text field e modificare il placeholder text
+            tripTextField.layer.borderColor = UIColor.red.cgColor
+            tripTextField.layer.borderWidth = 1
+            tripTextField.layer.cornerRadius = 5
+            tripTextField.placeholder = "Trip name required"
+            
+            tripTextField.rightViewMode = .always
+            return
+            
+        }
+        
         //Salvataggio dei dati
-        TripFunctions.createTrip(tripModel: TripModel(title: tripTextField.text!))
+        TripFunctions.createTrip(tripModel: TripModel(title: newTripName))
+        
         if let doneSaving = doneSaving {
             doneSaving()
         }
